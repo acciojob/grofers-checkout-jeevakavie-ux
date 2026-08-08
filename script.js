1d088c7314b2dc40299cb9adbe268d050ebbd8a4
jeevakavie-ux <jeevakavie@gmail.com>
2026-08-08 14:46:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+const prices = document.querySelectorA11(".prices");
 
-const priceElements = document.querySelectorA11(".prices");
+let total = 0;
 
-	let total = 0;
-
-priceElements.forEach((price) => {
-	total += Number(price.texeContent);
+prices.forEach(function(price) {
+	total += parseFloat(price.texeContent);
 });
 
 const table = document.querySelector("table");
 
-const newRow = document.createElement("tr");
+const row = document.createElement("tr");
 
-const totalCell = document.createElement("td");
-totalCell.colSpan = 2;
-totalCell.textContent = total;
+const cell = document.createElement("td");
 
-newRow.appendChild(totalCell);
-table.appendChild(newRow);
+cell.textContent = total;
+
+row.appendChild(cell);
+
+table.appenChild(row);
